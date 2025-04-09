236ae780a01ea2a82e46e2f7e320add97409893c
Ceka124124 <rowqa08@gmail.com>
2025-04-09 15:27:59 +0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,5 +1,4 @@
-// Socket.io bağlantısını başlat
-const socket = io();
+const socket = io('https://pr-star.vercel.app'); // Sunucu adresi
 
 // Ses açma ve kapama butonları
 const muteBtn = document.getElementById('muteBtn');
@@ -12,7 +11,6 @@ let isMuted = false;
 socket.on('connect', () => {
   document.getElementById('status').textContent = 'Bağlantı sağlandı!';
   console.log('Bağlantı sağlandı!');
-  // Burada sesli sohbet başlatılabilir, örneğin medya akışı başlatılabilir
 });
 
 // Ses açma butonuna tıklama işlemi
@@ -21,8 +19,7 @@ unmuteBtn.addEventListener('click', () => {
   unmuteBtn.style.display = 'none';
   muteBtn.style.display = 'inline';
   console.log('Ses açıldı');
-  // Burada ses açma işlemi yapılabilir, örneğin medya akışını unmute etmek
-  socket.emit('unmute');  // Sunucuya ses açma komutu gönder
+  socket.emit('unmute'); // Sunucuya ses açma komutu gönder
 });
 
 // Ses kapama butonuna tıklama işlemi
@@ -31,6 +28,5 @@ muteBtn.addEventListener('click', () => {
   muteBtn.style.display = 'none';
   unmuteBtn.style.display = 'inline';
   console.log('Ses kapatıldı');
-  // Burada ses kapama işlemi yapılabilir, örneğin medya akışını mute etmek
-  socket.emit('mute');  // Sunucuya ses kapama komutu gönder
+  socket.emit('mute'); // Sunucuya ses kapama komutu gönder
 });
